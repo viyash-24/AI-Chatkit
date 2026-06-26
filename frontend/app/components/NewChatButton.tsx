b022a2d9ef00fe11dd51b519d6108d20d52885b7
viyash-24 <viyashviyashan2417@gmail.com>
2026-06-26 08:47:35 +0530

    interface created

--- frontend/app/components/NewChatButton.tsx
+++ frontend/app/components/NewChatButton.tsx
@@ -1,3 +1,8 @@
 import React from 'react';
 import { Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
+
+interface NewChatButtonProps {
+  collapsed: boolean;
+  onClick: () => void;
+}
