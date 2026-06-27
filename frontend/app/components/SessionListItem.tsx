3e176cdd29759dbdf26315d581a964bf27e7c4ec
viyash-24 <viyashviyashan2417@gmail.com>
2026-06-27 20:17:12 +0530

    interface cretaed

--- frontend/app/components/SessionListItem.tsx
+++ frontend/app/components/SessionListItem.tsx
@@ -1,3 +1,8 @@
 import React from 'react';
 import { Dropdown, Menu as AntMenu, Button } from 'antd';
 import { EllipsisOutlined } from '@ant-design/icons';
+
+interface SessionListItemProps {
+  session: { threadId: string; name: string; lastUpdated: number };
+  onDelete: (threadId: string) => void;
+}
