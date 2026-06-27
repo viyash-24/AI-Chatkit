caee790c6de3a6322cd44a3c3f20a1115cd937cb
viyash-24 <viyashviyashan2417@gmail.com>
2026-06-27 20:21:46 +0530

    interface cretaed

--- frontend/app/components/SiderComponent.tsx
+++ frontend/app/components/SiderComponent.tsx
@@ -2,3 +2,14 @@ import React from 'react';
 import { Layout, Menu } from 'antd';
 import NewChatButton from './NewChatButton';
 import { useLayoutContext } from '../layout-context'
+
+
+interface SiderComponentProps {
+  collapsed: boolean;
+  onCollapse: (collapsed: boolean) => void;
+  sessions: Array<{ threadId: string; name: string; lastUpdated: number }>;
+  handleDeleteSession: (threadId: string) => void;
+  handlerNewChat: () => void;
+  items: Array<{ key: string; label: React.ReactNode }>;
+  onSelectSession: (key: string) => void;
+}
