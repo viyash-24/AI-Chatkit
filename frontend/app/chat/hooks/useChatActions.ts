08e4408ec30227f117a4ed1c597cf59ed0aa2d4f
viyash-24 <viyashviyashan2417@gmail.com>
2026-06-30 12:22:07 +0530

    inteface created

--- frontend/app/chat/hooks/useChatActions.ts
+++ frontend/app/chat/hooks/useChatActions.ts
@@ -1,2 +1,9 @@
 import { SetStateAction } from 'react';
 import { Message } from '../types/chat.types';
+
+interface UseChatActionsProps {
+  setMessages: (value: SetStateAction<Message[]>) => void;
+  setInput: (value: SetStateAction<string>) => void;
+  isStreaming: boolean;
+  setIsStreaming: (value: boolean) => void;
+}
