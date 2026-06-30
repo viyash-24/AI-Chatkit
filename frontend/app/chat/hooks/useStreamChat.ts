e334a3e062dca902798a052002455dfae747b789
viyash-24 <viyashviyashan2417@gmail.com>
2026-06-30 12:25:37 +0530

    inteface created

--- frontend/app/chat/hooks/useStreamChat.ts
+++ frontend/app/chat/hooks/useStreamChat.ts
@@ -1,2 +1,11 @@
 import { message } from "antd";
 import { Message } from "../types/chat.types";
+
+interface UseStreamChatProps {
+  currentThreadId: string;
+  agentId: string;
+  setMessages: (messages: Message[]) => void;
+  isStreaming: boolean;
+  setIsStreaming: (value: boolean) => void;
+}
+
