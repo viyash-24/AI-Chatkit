cdbce70fea772e3a85c40c505e710649ef2cce7c
viyash-24 <viyashviyashan2417@gmail.com>
2026-07-23 22:07:41 +0530

    chatcomponent module created

--- frontend/app/chat/components/ChatComponent.tsx
+++ frontend/app/chat/components/ChatComponent.tsx
@@ -6,3 +6,19 @@ import { Message, ChatComponentProps } from '../types/chat.types';
 import { useStreamChat } from '../hooks/useStreamChat';
 import MessageBubble from '../components/MessageBubble';
 import useChatActions from '../hooks/useChatActions';
+
+const ChatComponent: React.FC<ChatComponentProps> = ({
+  threadId,
+}) => {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isStreaming, setIsStreaming] = useState(false);
+  const messagesEndRef = useRef(null);
+  const { agentId, setAgentId, currentThreadId, setCurrentThreadId } = useLayoutContext()
+
+  useEffect(() => {
+    if(threadId){
+      setCurrentThreadId(threadId)
+    }
+  });
+  
