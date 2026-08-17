72d3130c91d19959ff2c9d5d486cd80f8a34e62d
viyash-24 <viyashviyashan2417@gmail.com>
2026-08-17 20:59:31 +0530

    agentinfo class is created

--- backend/app/ai/agent/agents.py
+++ backend/app/ai/agent/agents.py
@@ -4,3 +4,18 @@ from langgraph.graph.state import CompiledStateGraph
 from pydantic import BaseModel, Field
 from ai.agent.oa_assistant import oa_assistant
 from ai.agent.multi_agent import supervisor_agent
+
+
+DEFAULT_AGENT = "oa-assistant"
+
+class AgentInfo(BaseModel):
+    """Info about an available agent."""
+
+    key: str = Field(
+        description="Agent key.",
+        examples=["oa-assistant"],
+    )
+    description: str = Field(
+        description="Description of the agent.",
+        examples=["A oa assistant for company"],
+    )
