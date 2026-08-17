aad3c83ddc63a65041546140edd23c8e86aa0c31
viyash-24 <viyashviyashan2417@gmail.com>
2026-08-17 20:54:02 +0530

    class model

--- backend/tests/db/SqlModelTest.py
+++ backend/tests/db/SqlModelTest.py
@@ -3,3 +3,9 @@ from typing import Annotated, AsyncGenerator
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, SQLModel, select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+    secret_name: str = Field()
