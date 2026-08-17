6f99c38c26911657a72ff89dd50320558ebc48c2
viyash-24 <viyashviyashan2417@gmail.com>
2026-08-17 21:02:34 +0530

    import file

--- backend/app/ai/tools/oa_tools.py
+++ backend/app/ai/tools/oa_tools.py
@@ -1,1 +1,11 @@
 from langchain_core.tools import BaseTool, tool
+
+from db.models.employee import Employee
+from db.repository.employee_repo import EmployeeRepository
+from db.models.department import Department
+from db.repository.department_repo import DepartmentRepository
+from db.database import async_session_maker
+from dataclasses import dataclass, asdict
+from fastapi import Depends
+from ai.rag.chromaClient import hand_book_vector_store
+
