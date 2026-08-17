ff48f60450edd3f1b44cb036b733f8996c54a7e9
viyash-24 <viyashviyashan2417@gmail.com>
2026-08-17 21:01:21 +0530

    import file

--- backend/app/ai/rag/chromaClient.py
+++ backend/app/ai/rag/chromaClient.py
@@ -1,3 +1,8 @@
 import chromadb
 from chromadb.config import Settings
 from langchain_ollama import OllamaEmbeddings
+
+from langchain_chroma import Chroma
+from core.config import settings
+from chromadb.api.shared_system_client import SharedSystemClient
+
