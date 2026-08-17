3d8cc65e5286e134d8b6fc97ed767dd01f46521c
viyash-24 <viyashviyashan2417@gmail.com>
2026-08-17 20:55:38 +0530

    table create def create

--- backend/tests/db/SqlModelTest.py
+++ backend/tests/db/SqlModelTest.py
@@ -21,3 +21,7 @@ engine = create_async_engine(sqlite_url)
 async_session_maker = async_sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
 )
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
